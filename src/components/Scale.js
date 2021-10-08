f5a82230785a1e38ccc5d0b68ead6153a1cbacdf
João Pedrosa <jrcspp@hotmail.com>
2021-10-08 01:41:40 +0100

    final

--- src/components/Scale.js
+++ src/components/Scale.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { useState, useEffect } from "react";
+
 
 let rand1;
 let randScale;
